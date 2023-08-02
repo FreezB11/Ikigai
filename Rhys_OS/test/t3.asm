7b2bbfb64a97281923a6a08682d33d9886013107
FreezB11 <yashrajrao4@gmail.com>
2023-08-03 00:32:30 +0530

    test

--- Rhys_OS/test/t3.asm
+++ Rhys_OS/test/t3.asm
@@ -1,12 +1,11 @@
 @ org 0x7c00 ;offset
 
-jmp $
 
 mov al, 13h
 mov ah, 00
 int 10h
 
 
-
+jmp $
 times 510 - ($-$$) db 0 ; 3bytes from previous line + 510 bytes - 3 bytes = 510bytes
 db 0x55, 0xaa ; last 2 bytes of the boot sector
