5fd87b11554ea23c865ebc155d61c84581fecc7f
FreezB11 <yashrajrao4@gmail.com>
2023-08-03 21:09:24 +0530

    update

--- Ts/src/crypto/encrypt.ts
+++ Ts/src/crypto/encrypt.ts
@@ -7,14 +7,15 @@ const publicKey = Buffer.from(
   fs.readFileSync("src/key/public.pem", { encoding: "utf-8" })
 );
 
-const encryptedData = crypto.publicEncrypt(
-  {
-    key: publicKey,
-    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-    oaepHash: "sha256",
-  },
-  Buffer.from(dataToEncrypt)
-);
-
-const data = encryptedData.toString("base64")
-console.log(data)
+function encrypt(val:string):string{
+  const encryptedData = crypto.publicEncrypt(
+    {
+      key: publicKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    Buffer.from(val)
+  );
+  const data = encryptedData.toString("base64")
+  return data
+}
