c2af173ff3e0acd978e148b21beff0dc3005ba11
FreezB11 <yashrajrao4@gmail.com>
2023-08-03 13:04:52 +0530

    just simple

--- Ts/src/crypto/keygen.ts
+++ Ts/src/crypto/keygen.ts
@@ -2,24 +2,14 @@ import * as crypto from 'crypto'
 import* as fs from 'fs'
 
 const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
-  // The standard secure default length for RSA keys is 2048 bits
   modulusLength: 2048,
 });
-
-// *********************************************************************
-//
-// To export the public key and write it to file:
 
 const exportedPublicKeyBuffer = publicKey.export({
   type: "pkcs1",
   format: "pem",
 });
 fs.writeFileSync("src/key/public.pem", exportedPublicKeyBuffer, { encoding: "utf-8" });
-// *********************************************************************
-
-// *********************************************************************
-//
-// To export the private key and write it to file
 
 const exportedPrivateKeyBuffer = privateKey.export({
   type: "pkcs1",
