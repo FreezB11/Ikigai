3317f469dfbf5d4d31e816a7e09bae2461293f9c
FreezB11 <yashrajrao4@gmail.com>
2023-08-04 00:46:50 +0530

    home page

--- Ts/src/view/home.ts
+++ Ts/src/view/home.ts
@@ -12,6 +12,7 @@ const home = (req: Request, res: Response, next: NextFunction) => {
         <link rel="stylesheet" type="text/css" href="css/home_page.css">
     </head>
     <body>
+    <script src="js/home_page.js"></script>
     </body>
     </html>
     `)
