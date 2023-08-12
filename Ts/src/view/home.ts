f0c6d4e480489e8ae126b7a38a6445f35ec23378
FreezB11 <yashrajrao4@gmail.com>
2023-08-12 15:13:41 +0530

    link change

--- Ts/src/view/home.ts
+++ Ts/src/view/home.ts
@@ -9,13 +9,12 @@ const home = (req: Request, res: Response, next: NextFunction) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-        <script src="js/home_page.js">
+        <script src="js/home_page.js"></script>
         <link rel="stylesheet" type="text/css" href="css/home_page.css">
     </head>
     <body>
     <canvas id="canva"></canvas>
-    
-    </script>
+
     </body>
     </html>
     `)
