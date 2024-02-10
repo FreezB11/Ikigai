60ba6a6cfc4acc88ed3fb8d8ea51570d0a006bf5
FreezB11 <yashrajrao4@gmail.com>
2024-02-10 21:33:21 +0530

    linking

--- ipa/ipa.ts
+++ ipa/ipa.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express'
 import * as http from 'http'
 import * as color from 'colors'
+import logging from './config/logging'
 
 color.enable()
 
@@ -8,7 +9,18 @@ const NAMESPACE = 'Server'
 const app = express()
 const httpServer = http.createServer(app)
 
+app.use((req, res, next) => {
+    /** Log the req */
+    logging.info(NAMESPACE.blue, `METHOD: [${req.method.red}] - URL: [${req.url.green}] - IP: [${req.socket.remoteAddress}]`);
+    res.on('finish', () => {
+        /** Log the res */
+        logging.info(NAMESPACE.blue, `METHOD: [${req.method.red}] - URL: [${req.url.green}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`);
+    })
+    next();
+})
 
+app.use(express.json());       
+app.use(express.urlencoded({extended: true}));
 
 
 export = app; httpServer;
