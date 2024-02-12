76ed40324db2c1617d98b5d856a2991d4ff6f7a4
FreezB11 <yashrajrao4@gmail.com>
2024-02-12 06:21:25 +0530

    change

--- ipa/controller/db.controller.ts
+++ ipa/controller/db.controller.ts
@@ -1,10 +1,10 @@
 import mongoose, {Schema} from "mongoose";
 import UsrModel from "../models/usr.model";
 
-const get_usr = ()=>UsrModel.find();
-const usrby_mail = (email:String)=> UsrModel.findOne({email});
-const usr_byseesiontoken = (sessionToken: String)=> UsrModel.findOne({
-    'authentication.sessionToken':sessionToken,
-})
-
-const usr_byid = (id:String)=> UsrModel.findOne(id);
+export const getUsers = () => UsrModel.find();
+export const getUserByEmail = (email: string) => UsrModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) => UsrModel.findOne({ 'authentication.sessionToken': sessionToken });
+export const getUserById = (id: string) => UsrModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UsrModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UsrModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UsrModel.findByIdAndUpdate(id, values);
