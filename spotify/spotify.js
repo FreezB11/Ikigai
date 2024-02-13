c5b138457579aa26ccb2759a32d25d665909cffe
FreezB11 <yashrajrao4@gmail.com>
2024-02-13 20:09:37 +0530

    radom

--- spotify/spotify.js
+++ spotify/spotify.js
@@ -8,7 +8,26 @@ fs.readFile("spotify20.json", function(err, data) {
  
     // Converting to JSON 
     const dat = JSON.parse(data); 
-    const arrayData = dat.map(item => item.master_metadata_track_name)
-    console.log(arrayData)
+    const songname = dat.map(item => item.master_metadata_track_name)
+    const tim = dat.map(item=>item.ts)
+
+    const config = [{ tim :songname},]
+
+    fs.writeFile("names.json", JSON.stringify(config, null, 2), (error) => {
+        if (error) {
+          console.log('An error has occurred ', error);
+          return;
+        }
+        console.log('Data written successfully to disk');
+      });
+
+    console.log(songname)
 });
 
+// fs.writeFile("names.json", JSON.stringify(config, null, 2), (error) => {
+//     if (error) {
+//       console.log('An error has occurred ', error);
+//       return;
+//     }
+//     console.log('Data written successfully to disk');
+//   });
