9e2f30be2c3f63ea0a66876286bb5e94612a857b
FreezB11 <yashrajrao4@gmail.com>
2024-02-14 07:50:37 +0530

    working on login/register

--- ipa/controller/auth.controller.ts
+++ ipa/controller/auth.controller.ts
@@ -4,9 +4,27 @@ import { getUserByEmail, createUser } from './db.controller';
 
 const login = async (req: Request, res: Response) => {
   try {
-    const {name,email,password}=req.body
+    const {email,password}=req.body
+    if(!email||!password){
+      res.status(400).json({"ERROR":"looks like u r missing smtg!!"})
+    }
+    const user = await getUserByEmail(email).select('+authentication.password');
+    if(!user){
+      res.status(400).json({"ERR":"looks like u didnt register dude"})
+    }
 
-   
+    console.log(user?.authentication?.password)
+    console.log(password)
+
+    if(user?.authentication?.password == password){
+      res.status(200).json({"ERR":"checking.....pswd is ok"})
+    }else{
+      console.log("testing....... pswd is wrong")
+      res.status(404).json({"msg":"sry"})
+    }
+
+    console.log("welcome")
+    // return res.status(200).json({"messasge":"welcome dude"})
 
   } catch (error) {
     console.log(error);
