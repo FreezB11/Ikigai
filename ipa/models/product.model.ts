1ddf6a2f681a0953c373997ee4880757f57762db
FreezB11 <yashrajrao4@gmail.com>
2024-02-17 16:20:37 +0530

    product

--- ipa/models/product.model.ts
+++ ipa/models/product.model.ts
@@ -2,9 +2,10 @@ import mongoose, {Schema,Model,model} from "mongoose";
 
 const productModel = new Schema({
     merchant_id:{type:String,required:true},
+    product_id:{type:String,required:true},
     name:{type:String,required:true},
     price:{type:Number,required:true},
-    category_id:Number,
+    category_id:String,
     Images:[String]
 },{collection:'products'})
 
--- ipa/routr/routr.product.ts
+++ ipa/routr/routr.product.ts
@@ -9,6 +9,6 @@ enum routr{
     delete = '/delete/:id'
 }
 product_routr.post(routr.add,controllr.add)
-
+product_routr.post(routr.search,controllr.search)
 
 export default product_routr
