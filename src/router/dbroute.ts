f5b028bde3235668c113b5adede9fbf6283c137f
FreezB11 <yashrajrao4@gmail.com>
2024-07-08 22:26:38 +0530

    tried smtg but again failed

--- src/router/dbroute.ts
+++ src/router/dbroute.ts
@@ -1,5 +1,9 @@
 import {Request, Response, Router} from 'express'
-
+const db = './src/db';
+import * as fs from 'fs'
+import * as path from 'path'
+import * as crypto from 'crypto'
+import * as yaml from 'yaml'
 const router = Router()
 
 enum func{
@@ -19,7 +23,16 @@ router.get(func.get,(_:Request,res:Response)=>{
 })
 
 router.get(func.all,(_:Request, res:Response) =>{
-    res.send('hello')
+    var usr_list: any = [];
+    fs.readdir(db, (err: any, files: any[]) => {
+        files.forEach(file => {
+            usr_list.push(file)
+            //res.status(400).json({message:file}) ///// here decryption will be done
+        });
+      });
+
+    res.status(400).json({message:usr_list})
+    // res.send('hello')
 })
 
 export default router
